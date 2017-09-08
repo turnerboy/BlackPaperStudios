a4d584ae7b739761f9a15af6c9758ccea7a13ee4
Sri Ganesh <sri.turner98@gmail.com>
2017-09-08 09:00:34 +0530

    Added Files

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+  var fadeShow = $(".background").fadeShow({
+    correctRatio: true,
+    shuffle: true,
+    speed: 2000,
+    images: ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg']
+  });
+});
